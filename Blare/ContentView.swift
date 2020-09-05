ea1204351ebba1c09cdcdf482d22a20920c53782
Michael Goodnow <mmgoodnow@gmail.com>
2020-09-05 19:30:41 -0400

    Initial Commit

---
+++ Blare/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Blare
+//
+//  Created by Michael Goodnow on 9/5/20.
+//  Copyright © 2020 Michael Goodnow. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
